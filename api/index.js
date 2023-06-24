b7ad8f513c1f112fe63a4a9a599b84a67299dc1f
Shivendra Singh <shivendrasingh@Shivendras-Mac-mini.local>
2023-06-24 10:05:06 +0530

    Secured password by bcryptjs

--- api/index.js
+++ api/index.js
@@ -2,9 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const User = require("./model/User.js");
+const bcrypt = require("bcryptjs");
 const app = express();
 const PORT = 8080;
 const mongoose = require("mongoose");
+const bcryptSalt = bcrypt.genSaltSync(12);
 
 mongoose.connect(process.env.MONGO_URL);
 
@@ -16,7 +18,7 @@ app.post("/api/register", async (req, res) => {
   await User.create({
     name,
     email,
-    password,
+    password: bcrypt.hashSync(password, bcryptSalt),
   });
   res.json("OK");
 });
@@ -25,7 +27,8 @@ app.post("/api/login", async (req, res) => {
   const { email, password } = req.body;
   const userData = await User.findOne({ email });
   if (userData) {
-    if (password === userData.password) {
+    const passOk = bcrypt.compareSync(password, userData.password);
+    if (passOk) {
       res.status(200).json({ success: true, msg: "Login Successful" });
     } else {
       res.status(500).json({ error: "Incorrect Password" });
